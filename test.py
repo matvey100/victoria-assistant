ba105bdaba8e78be1d3eb5f3d39625fd31b440df
matve <matvey.seva81@ya.ru>
2022-11-21 20:38:59 +0300

    day 1.1

---
+++ test.py
@@ -0,0 +1,17 @@
+import pyttsx3
+
+tts = pyttsx3.init() # Инициализировать голосовой движок.
+voices = tts.getProperty('voices')
+for voice in voices:
+
+    print('=======')
+
+    print('Имя: %s' % voice.name)
+
+    print('ID: %s' % voice.id)
+
+    print('Язык(и): %s' % voice.languages)
+
+    print('Пол: %s' % voice.gender)
+
+    print('Возраст: %s' % voice.age)
