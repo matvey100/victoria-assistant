6d24e45d611dfe9daade1e040c827c8f1f4ee265
matve <matvey.seva81@ya.ru>
2022-11-23 23:13:28 +0300

    commit day 3.1 * removed english because bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''
+"""
 Прототип голосового помощника
 Для работы требуются следующие зависимости: pip install pyaudio wikipedia-api google pyttsx3 chardet SpeechRecognition
 Также требуется установить следующие библиотеки из репозитория https://github.com/RHVoice/RHVoice/blob/master/doc/ru/Binaries.md
@@ -9,7 +9,7 @@ SAPI 5 для Windows / NVDA для Linux и Macintosh
 
 All rights reserved.
 2022
-'''
+"""
 # from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
 import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
 # import wave  # создание и чтение аудиофайлов формата wav
@@ -17,7 +17,7 @@ import speech_recognition  # распознавание пользователь
 # import os  # работа с файловой системой
 import pyttsx3  # Синтез речи
 import webbrowser  # Работа с браузером
-import chardet  # определение языка речи
+# import chardet  # определение языка речи
 # from googlesearch import search  # поиск в google
 import traceback  # вывод traceback без остановки работы программы при отлове исключений
 import wikipediaapi  # найти в википедии
@@ -32,13 +32,12 @@ tts.setProperty('voice', 'ru')
 # Попробовать установить предпочтительный голос
 for voice in voices:
     ru = voice.id.find('RHVoice\Anna')  # Найти Анну от RHVoice
-    if ru > -1: # если нашли, выбираем этот голос
+    if ru > -1:  # если нашли, выбираем этот голос
         tts.setProperty('voice', voice.id)
 
 tts.say('Привет, Создатель!')
 tts.runAndWait()
-# tts.say('Hi, user!')
-# tts.runAndWait()
+
 
 def play_voice_assistant_speech(text_to_speech):
     """
@@ -47,6 +46,7 @@ def play_voice_assistant_speech(text_to_speech):
     """
     tts.say(str(text_to_speech))
     tts.runAndWait()
+
 
 def record_and_recognize_audio(*args: tuple):
     """
@@ -73,6 +73,7 @@ def record_and_recognize_audio(*args: tuple):
             print("Check your Internet Connection, please")
         return recognized_data
 
+
 def search_for_video_on_youtube(*args: tuple):
     """
     Поиск видео на YouTube с автоматическим открытием ссылки на список результатов
@@ -85,40 +86,27 @@ def search_for_video_on_youtube(*args: tuple):
 
     # для мультиязычных голосовых ассистентов лучше создать
     # отдельный класс, который будет брать перевод из JSON-файла
-    if lang == 'Russian':
-        print("Вот что я нашла по запросу " + search_term + "в ютуб.")
-        play_voice_assistant_speech("Вот что я нашла по запросу " + search_term + "в ютуб.")
-    else:
-        print("Here is what I found for " + search_term + "on youtube")
-        play_voice_assistant_speech("Here is what I found for " + search_term + "on youtube")
+    print("Вот что я нашла по запросу " + search_term + "в ютуб.")
+    play_voice_assistant_speech("Вот что я нашла по запросу " + search_term + "в ютуб.")
+
 
 def play_greetings(*args: tuple):
-    '''Приветствие пользователя'''
-    if lang == 'Russian':
-        print('Привет, пользователь! Я - голосовой помощник Виктория. Чем я могу пом+очь вам?')
-        play_voice_assistant_speech('Привет, пользователь! Я - голосовой помощник Виктория. Чем я могу пом+очь вам?')
-    else:
-        print('Hello, user! I am - voice assistant Victoria. How can I help you?')
-        play_voice_assistant_speech('Hello, user! I am - voice assistant Victoria. How can I help you?')
+    """Приветствие пользователя"""
+    print('Привет, пользователь! Я - голосовой помощник Виктория. Чем я могу пом+очь вам?')
+    play_voice_assistant_speech('Привет, пользователь! Я - голосовой помощник Виктория. Чем я могу пом+очь вам?')
+
 
 def play_farewell_and_quit(*args: tuple):
-    """
-    Проигрывание прощательной речи и выход
-    """
-    if lang == 'Russian':
-        print("До свидания! Хорошего дня!")
-        play_voice_assistant_speech("До свидания! Хорошего дня!")
-    else:
-        print('Goodbye! Have a good day!')
-        play_voice_assistant_speech('Goodbye! Have a good day!')
+    """Проигрывание прощальной речи и выход"""
+    print("До свидания! Хорошего дня!")
+    play_voice_assistant_speech("До свидания! Хорошего дня!")
     tts.stop()
     quit()
 
+
 def search_for_term_on_google(*args: tuple):
-    """
-    Поиск в Google с автоматическим открытием ссылок (на список результатов и на сами результаты, если возможно)
-    :param args: фраза поискового запроса
-    """
+    """Поиск в Google с автоматическим открытием ссылок (на список результатов и на сами результаты, если возможно)
+    :param args: фраза поискового запроса"""
     if not args[0]: return
     search_term = " ".join(args[0])
     # открытие ссылки в браузере
@@ -144,18 +132,13 @@ def search_for_term_on_google(*args: tuple):
     #     traceback.print_exc()
     #     return
     # print(search_results)
-    if lang == 'Russian':
-        print(('Вот что я нашла по запросу {}.').format(search_term))
-        play_voice_assistant_speech(('Вот что я нашла по запросу {}.').format(search_term))
-    else:
-        print(("Here is what I found for {} on google").format(search_term))
-        play_voice_assistant_speech(("Here is what I found for {} on google").format(search_term))
+    print('Вот что я нашла по запросу {}.'.format(search_term))
+    play_voice_assistant_speech('Вот что я нашла по запросу {}.'.format(search_term))
+
 
 def search_for_definition_on_wikipedia(*args: tuple):
-    """
-    Поиск в Wikipedia определения с последующим озвучиванием результатов и открытием ссылок
-    :param args: фраза поискового запроса
-    """
+    """Поиск в Wikipedia определения с последующим озвучиванием результатов и открытием ссылок
+    :param args: фраза поискового запроса"""
     if not args[0]: return
     search_term = " ".join(args[0])
     # установка языка (в данном случае используется язык, на котором говорит ассистент)
@@ -164,24 +147,16 @@ def search_for_definition_on_wikipedia(*args: tuple):
     wiki_page = wiki.page(search_term)
     try:
         if wiki_page.exists():
-            if lang == 'Russian':
-                print(("Вот что я нашла по запросу {} в Википедии").format(search_term))
-                play_voice_assistant_speech(("Вот что я нашла по запросу {} в Википедии").format(search_term))
-            else:
-                print(("Here is what I found for {} on Wikipedia").format(search_term))
-                play_voice_assistant_speech(("Here is what I found for {} on Wikipedia").format(search_term))
+            print("Вот что я нашла по запросу {} в Википедии".format(search_term))
+            play_voice_assistant_speech("Вот что я нашла по запросу {} в Википедии".format(search_term))
             webbrowser.get().open(wiki_page.fullurl)
             # чтение ассистентом первых двух предложений summary со страницы Wikipedia
             # (могут быть проблемы с мультиязычностью)
             play_voice_assistant_speech(wiki_page.summary.split(".")[:2])
         else:
             # открытие ссылки в браузере в случае, если на Wikipedia не удалось найти ничего по запросу
-            if lang == 'Russian':
-                print("Такой информации нет в Википедии, вот результаты в google.")
-                play_voice_assistant_speech("Такой информации нет в Википедии, вот результаты в google.")
-            else:
-                print(("Can't find {} on Wikipedia. But here is what I found on google").format(search_term))
-                play_voice_assistant_speech(("Can't find {} on Wikipedia. But here is what I found on google").format(search_term))
+            print("Такой информации нет в Википедии, вот результаты в google.")
+            play_voice_assistant_speech("Такой информации нет в Википедии, вот результаты в google.")
             url = "https://google.com/search?q=" + search_term
             webbrowser.get().open(url)
     # поскольку все ошибки предсказать сложно, то будет произведен отлов с последующим выводом без остановки программы
@@ -191,31 +166,22 @@ def search_for_definition_on_wikipedia(*args: tuple):
         traceback.print_exc()
         return
 
+
 def flip_a_coin(*args: tuple):
     l = random.randint(0, 2)
     if l == 0:
-        if lang == 'Russian':
-            print('Орёл!')
-            play_voice_assistant_speech('Орёл!')
-        else:
-            print('Heads!')
-            play_voice_assistant_speech('Heads!')
+        print('Орёл!')
+        play_voice_assistant_speech('Орёл!')
     else:
-        if lang == 'Russian':
-            print('Решка!')
-            play_voice_assistant_speech('Решка!')
-        else:
-            print('Tails!')
-            play_voice_assistant_speech('Tails!')
+        print('Решка!')
+        play_voice_assistant_speech('Решка!')
     return
 
+
 def name_trigger(*args: tuple):
-    if lang == 'Russian':
-        print('Чем я могу пом+очь?')
-        play_voice_assistant_speech('Чем я могу пом+очь?')
-    else:
-        print('How can I help you?')
-        play_voice_assistant_speech('How can I help you?')
+    print('Чем я могу пом+очь?')
+    play_voice_assistant_speech('Чем я могу пом+очь?')
+
 
 def execute_command_with_name(command_name: str, *args: list):
     """
@@ -228,15 +194,8 @@ def execute_command_with_name(command_name: str, *args: list):
         if command_name in key:
             commands[key](*args)
         else:
-            # if lang == 'Russian':
-            #     play_voice_assistant_speech('Такой команды не существует.')
-            #     print('Такой команды не существует.')
-            #     pass
-            # else:
-            #     play_voice_assistant_speech("Command not found")
-            #     print("Command not found")
-            #     pass
             pass
+
 
 '''Команды для помощника'''
 commands = {
@@ -262,9 +221,6 @@ if __name__ == "__main__":
         voice_input = record_and_recognize_audio()
         # os.remove("microphone-results.wav")
         print(voice_input)
-        about_lang = dict.values(chardet.detect(voice_input[0].encode('cp1251')))  # определение распознаваемого языка
-        lang = [*about_lang][2]
-        # print(lang)
 
         # отделение комманд от дополнительной информации (аргументов)
         voice_input = voice_input.split(" ")
